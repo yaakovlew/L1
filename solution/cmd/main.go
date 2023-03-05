3c42635f17503f8122ec66507d81806f21377e8c
yaakovlew <andrei252002@mail.ru>
2023-03-05 23:31:14 +0300

    16

--- solution/cmd/main.go
+++ solution/cmd/main.go
@@ -1,4 +1,8 @@
 package main
 
+import "solution/write_tasks/sixteenth_task"
+
 func main() {
+	mas := []int{5, 4, 3, 6, 2}
+	sixteenth_task.Sixteen(mas)
 }
--- solution/write_tasks/sixteenth_task/sixteenth_task.go
+++ solution/write_tasks/sixteenth_task/sixteenth_task.go
@@ -1,7 +1,9 @@
 package sixteenth_task
 
+// быстрая сортировка
 func quickSort(arr []int, left, right int) {
 	if left < right {
+		//выбор границы подмассива
 		pivot := partition(arr, left, right)
 		quickSort(arr, left, pivot-1)
 		quickSort(arr, pivot+1, right)
@@ -9,14 +11,17 @@ func quickSort(arr []int, left, right int) {
 }
 
 func partition(arr []int, left, right int) int {
+	//берем самый правый элемент
 	pivot := arr[right]
 	i := left - 1
 	for j := left; j < right; j++ {
+		//если правый элемент больше чем текущий, то увеличиваем i
 		if arr[j] < pivot {
 			i++
 			arr[i], arr[j] = arr[j], arr[i]
 		}
 	}
+	//меняем местами самый правый и то место, где все стоящие слева будут меньше него, а справа - больше
 	arr[i+1], arr[right] = arr[right], arr[i+1]
 	return i + 1
 }
