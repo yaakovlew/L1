49c2dedb48f0625fa037ba180a6404bf0d89fa65
yaakovlew <andrei252002@mail.ru>
2023-03-07 13:52:02 +0300

    some changes

--- solution/write_tasks/seventh_task/seventh_task.go
+++ solution/write_tasks/seventh_task/seventh_task.go
@@ -5,20 +5,20 @@ import (
 	"sync"
 )
 
-//Структура map
+// Структура map
 type NewMap struct {
 	mx sync.Mutex
 	m  map[string]int
 }
 
-//конструктор
+// конструктор
 func NewNewMap() *NewMap {
 	return &NewMap{
 		m: make(map[string]int),
 	}
 }
 
-//Метод получения значения по ключу
+// Метод получения значения по ключу
 func (c *NewMap) Load(key string) (int, bool) {
 	c.mx.Lock()
 	defer c.mx.Unlock()
@@ -26,18 +26,22 @@ func (c *NewMap) Load(key string) (int, bool) {
 	return val, ok
 }
 
-//Метод добавления значения по ключу
-func (c *NewMap) Store(key string, value int) {
+// Метод добавления значения по ключу
+func (c *NewMap) Store(key string, value int, w *sync.WaitGroup) {
+	defer w.Done()
 	c.mx.Lock()
 	defer c.mx.Unlock()
 	c.m[key] = value
 }
 
-//Пример использования
+// Пример использования
 func Seventh() {
 	mapka := NewNewMap()
-	go mapka.Store("hello", 2)
-	go mapka.Store("world", 3)
-	go mapka.Store("and you", 1)
+	w := &sync.WaitGroup{}
+	w.Add(3)
+	go mapka.Store("hello", 2, w)
+	go mapka.Store("world", 3, w)
+	go mapka.Store("and you", 1, w)
+	w.Wait()
 	fmt.Println(mapka)
 }
