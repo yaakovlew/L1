d8743ef38bbf5a436a41018ba9d20500404acb70
yaakovlew <andrei252002@mail.ru>
2023-03-08 15:20:39 +0300

    Some changes

--- solution/write_tasks/seventh_task/seventh_task.go
+++ solution/write_tasks/seventh_task/seventh_task.go
@@ -3,12 +3,13 @@ package seventh_task
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 // Структура map
 type NewMap struct {
-	mx sync.Mutex
-	m  map[string]int
+	mut sync.RWMutex
+	m   map[string]int
 }
 
 // конструктор
@@ -20,8 +21,6 @@ func NewNewMap() *NewMap {
 
 // Метод получения значения по ключу
 func (c *NewMap) Load(key string) (int, bool) {
-	c.mx.Lock()
-	defer c.mx.Unlock()
 	val, ok := c.m[key]
 	return val, ok
 }
@@ -29,9 +28,10 @@ func (c *NewMap) Load(key string) (int, bool) {
 // Метод добавления значения по ключу
 func (c *NewMap) Store(key string, value int, w *sync.WaitGroup) {
 	defer w.Done()
-	c.mx.Lock()
-	defer c.mx.Unlock()
+	c.mut.RLock()
+	defer c.mut.RUnlock()
 	c.m[key] = value
+	time.Sleep(3 * time.Second)
 }
 
 // Пример использования
@@ -40,6 +40,8 @@ func Seventh() {
 	w := &sync.WaitGroup{}
 	w.Add(3)
 	go mapka.Store("hello", 2, w)
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println(mapka.Load("hello"))
 	go mapka.Store("world", 3, w)
 	go mapka.Store("and you", 1, w)
 	w.Wait()
