fad7def492ac28ed8a4e08db2429f992e47f74d9
yaakovlew <andrei252002@mail.ru>
2023-03-08 23:42:20 +0300

    11

--- solution/write_tasks/eleventh_task/eleventh_task.go
+++ solution/write_tasks/eleventh_task/eleventh_task.go
@@ -2,21 +2,36 @@ package eleventh_task
 
 import (
 	"fmt"
-	mapset "github.com/deckarep/golang-set"
 )
 
-func Eleven() {
-	//Создание и инициализация первого мн-ва
-	required := mapset.NewSet()
-	required.Add("cooking")
-	required.Add("english")
-	required.Add("math")
-	required.Add("biology")
-	//Создание и инициализация второго мн-ва
-	sciences := mapset.NewSet()
-	sciences.Add("biology")
-	sciences.Add("chemistry")
-	//Пересечение этих мн-в
-	count := required.Intersect(sciences)
-	fmt.Println(count)
+type void struct{}
+
+// инициализация пустой структуры
+var empty void
+
+func Intersection(s1, s2 map[string]void) map[string]void {
+	set := make(map[string]void)
+	for v, _ := range s1 {
+		if _, exist := s2[v]; exist == true {
+			set[v] = empty
+		}
+	}
+	return set
+}
+
+func Eleventh(s1, s2 []string) {
+	//создание первого мн-ва
+	set1 := make(map[string]void)
+	//добавление элементов в мн-во
+	for _, v := range s1 {
+		set1[v] = empty
+	}
+	//создание второго мн-ва
+	set2 := make(map[string]void)
+	//добавление элементов в мн-во
+	for _, v := range s2 {
+		set2[v] = empty
+	}
+	//Результат
+	fmt.Println(Intersection(set1, set2))
 }
